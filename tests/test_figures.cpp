ff844a190ad039b4fb3e1d742b9f2158ac2cd760
Maxim <maximkutkov@gmail.com>
2025-11-08 11:09:48 +0000

    added tests

--- tests/test_figures.cpp
+++ tests/test_figures.cpp
@@ -1,8 +1,6 @@
-// tests/test_figures.cpp
-#include <cassert>
-#include <iostream>
+#include <gtest/gtest.h>
 #include <memory>
-
+#include <cmath>
 #include "figure_array.h"
 #include "rectangle.h"
 #include "rhombus.h"
@@ -10,75 +8,111 @@
 
 using D = double;
 
-void test_rectangle_basic() {
+//
+// ---------- RECTANGLE TESTS ----------
+//
+TEST(RectangleTest, BasicProperties) {
     Rectangle<D> r1(Point<D>{0,0}, Point<D>{2,0}, Point<D>{2,1}, Point<D>{0,1});
     Rectangle<D> r2 = r1; // copy
-    assert(r1 == r2);
-    assert(std::abs(r1.area() - 2.0) < 1e-9);
+
+    EXPECT_TRUE(r1 == r2);
+    EXPECT_NEAR(r1.area(), 2.0, 1e-9);
+
     auto c = r1.center();
-    assert(std::abs(c.x - 1.0) < 1e-9 && std::abs(c.y - 0.5) < 1e-9);
-    std::cout << "[OK] Rectangle basic\n";
+    EXPECT_NEAR(c.x, 1.0, 1e-9);
+    EXPECT_NEAR(c.y, 0.5, 1e-9);
 }
 
-void test_rhombus_basic() {
+TEST(RectangleTest, InequalityAfterChange) {
+    Rectangle<D> r1(Point<D>{0,0}, Point<D>{2,0}, Point<D>{2,1}, Point<D>{0,1});
+    Rectangle<D> r2(Point<D>{0,0}, Point<D>{1,0}, Point<D>{1,1}, Point<D>{0,1});
+    EXPECT_FALSE(r1 == r2);
+}
+
+TEST(RectangleTest, CloneWorks) {
+    Rectangle<D> r1(Point<D>{0,0}, Point<D>{2,0}, Point<D>{2,1}, Point<D>{0,1});
+    std::unique_ptr<Figure<D>> clone(r1.clone());
+    EXPECT_NEAR(static_cast<double>(*clone), r1.area(), 1e-9);
+}
+
+//
+// ---------- RHOMBUS TESTS ----------
+//
+TEST(RhombusTest, BasicProperties) {
     Rhombus<D> rh(Point<D>{0,0}, Point<D>{1,1}, Point<D>{2,0}, Point<D>{1,-1});
     double a = rh.area();
-    assert(a > 0);
+    EXPECT_GT(a, 0.0);
+
     auto c = rh.center();
-    assert(std::abs(c.x - 1.0) < 1e-9);
-    std::cout << "[OK] Rhombus basic\n";
+    EXPECT_NEAR(c.x, 1.0, 1e-9);
+    EXPECT_NEAR(c.y, 0.0, 1e-9);
 }
 
-void test_trapezoid_basic() {
+//
+// ---------- TRAPEZOID TESTS ----------
+//
+TEST(TrapezoidTest, BasicProperties) {
     Trapezoid<D> t(Point<D>{0,0}, Point<D>{3,0}, Point<D>{2,1}, Point<D>{1,1});
-    double a = t.area();
-    assert(a > 0.0);
+    EXPECT_GT(t.area(), 0.0);
+
     auto c = t.center();
-    assert(c.x > 0 && c.y >= 0);
-    std::cout << "[OK] Trapezoid basic\n";
+    EXPECT_GT(c.x, 0.0);
+    EXPECT_GE(c.y, 0.0);
 }
 
-void test_array_operations() {
+//
+// ---------- ARRAY TESTS ----------
+//
+TEST(ArrayTest, SharedPtrFiguresOperations) {
     Array<std::shared_ptr<Figure<D>>> arr;
     auto r = std::make_shared<Rectangle<D>>(Point<D>{0,0}, Point<D>{1,0}, Point<D>{1,1}, Point<D>{0,1});
     auto t = std::make_shared<Trapezoid<D>>(Point<D>{0,0}, Point<D>{3,0}, Point<D>{2,1}, Point<D>{1,1});
+
     arr.push_back(r);
     arr.push_back(std::move(t));
 
-    assert(arr.size() == 2);
+    EXPECT_EQ(arr.size(), 2);
     double total = arr.totalArea();
-    assert(total > 0);
+    EXPECT_GT(total, 0.0);
 
-    // erase
     arr.erase(0);
-    assert(arr.size() == 1);
+    EXPECT_EQ(arr.size(), 1);
 
-    // move
     Array<std::shared_ptr<Figure<D>>> arr2 = std::move(arr);
-    assert(arr.size() == 0 && arr2.size() == 1);
-    std::cout << "[OK] Array operations\n";
+    EXPECT_EQ(arr.size(), 0);
+    EXPECT_EQ(arr2.size(), 1);
 }
 
-void test_array_with_concrete_type() {
+TEST(ArrayTest, ConcreteRectangleType) {
     Array<Rectangle<D>> rects;
     Rectangle<D> R1(Point<D>{0,0}, Point<D>{2,0}, Point<D>{2,1}, Point<D>{0,1});
     Rectangle<D> R2(Point<D>{0,0}, Point<D>{1,0}, Point<D>{1,1}, Point<D>{0,1});
+
     rects.push_back(R1);
     rects.push_back(std::move(R2));
-    assert(rects.size() == 2);
+
+    EXPECT_EQ(rects.size(), 2);
     double total = rects.totalArea();
-    assert(total > 0);
+    EXPECT_GT(total, 0.0);
+
     rects.erase(1);
-    assert(rects.size() == 1);
-    std::cout << "[OK] Array<Rectangle> concrete type\n";
+    EXPECT_EQ(rects.size(), 1);
 }
 
-int main() {
-    test_rectangle_basic();
-    test_rhombus_basic();
-    test_trapezoid_basic();
-    test_array_operations();
-    test_array_with_concrete_type();
-    std::cout << "All tests passed!\n";
-    return 0;
+TEST(ArrayTest, TotalAreaWithDifferentFigures) {
+    Array<std::shared_ptr<Figure<D>>> arr;
+    arr.push_back(std::make_shared<Rectangle<D>>(Point<D>{0,0}, Point<D>{1,0}, Point<D>{1,1}, Point<D>{0,1}));
+    arr.push_back(std::make_shared<Rhombus<D>>(Point<D>{0,0}, Point<D>{1,1}, Point<D>{2,0}, Point<D>{1,-1}));
+    arr.push_back(std::make_shared<Trapezoid<D>>(Point<D>{0,0}, Point<D>{3,0}, Point<D>{2,1}, Point<D>{1,1}));
+
+    double total = arr.totalArea();
+    EXPECT_GT(total, 0.0);
+}
+
+//
+// ---------- MAIN ----------
+//
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
